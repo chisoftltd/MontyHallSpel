705495187ce9dea6df05275179b57dcc0d5e4da8
Chisoft <Chisoft@192.168.0.105>
2016-02-04 17:56:35 +0100

    new commit

---
+++ src/mvctictactoe/TicTacToeException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvctictactoe;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Benjamin Chinwe 2016
+ * To handle exceptions in TicTacToe
+ */
+public class TicTacToeException extends IOException{ 
+    public TicTacToeException (){
+        super();
+    }
+    public TicTacToeException (String message){
+        super(message);
+    }
+    public TicTacToeException (String message, Throwable cause){
+        super(message, cause);
+    }
+    
+    public TicTacToeException (Throwable cause){
+        super(cause);
+    }
+}
